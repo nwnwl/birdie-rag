21064f65868282190e2ac29e33ccca2bf665cd38
nwnwl <wn17782008094@outlook.com>
2026-05-24 20:36:40 +0800

    修复Header focus样式

--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -1,22 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Link to="/">
       <h1 className="sm:text-3xl text-2xl   flex items-center gap-2 ">
-        <span style={{ fontSize: '1em', verticalAlign: '-0.1em' }}>
+        <span style={{ fontSize: "1em", verticalAlign: "-0.1em" }}>
           <img
             src="/favicon.ico"
             style={{
-              width: '1.2em',
-              height: '1.2em',
-              display: 'inline-block',
-              objectFit: 'contain',
+              width: "1.2em",
+              height: "1.2em",
+              display: "inline-block",
+              objectFit: "contain",
             }}
             alt="bird emoji"
           />
-        </span>{' '}
-        <span className="text-amber-900/90 font-cause tracking-wider">Birdie</span>
+        </span>{" "}
+        <span className="text-amber-900/90 font-cause tracking-wider ">
+          Birdie
+        </span>
       </h1>
     </Link>
   );
